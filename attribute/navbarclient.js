7cd380d5acbfc1d75b80afb1e6a8b1c6060f4c8f
vicenzoradjaa-collab <vicenzoradjaa@gmail.com>
2026-07-13 18:56:51 +0700

    Update navbarclient.js

--- attribute/navbarclient.js
+++ attribute/navbarclient.js
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function() {
         { 
             id: 'profil', 
             label: 'Profil', 
-            url: '#', 
+            url: 'client-profile.html', 
             icon: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>'
         }
     ];
