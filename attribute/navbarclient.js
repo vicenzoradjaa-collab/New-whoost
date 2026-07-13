c68c0628270d37d67fe658c0b13d9745e12e21d9
vicenzoradjaa-collab <vicenzoradjaa@gmail.com>
2026-07-13 19:05:59 +0700

    Update navbarclient.js

--- attribute/navbarclient.js
+++ attribute/navbarclient.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function() {
             icon: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z"></path>'
         },
         { 
-            id: 'analytics', 
+            id: 'performa', 
             label: 'Analytics', 
             url: 'client-analytics.html', 
             icon: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path>'
