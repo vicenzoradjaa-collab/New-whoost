aa4a9002246a0f7113e49b912a637b0d69141fe1
vicenzoradjaa-collab <vicenzoradjaa@gmail.com>
2026-07-15 07:15:21 +0700

    Update navbaradmin.js

--- attribute/navbaradmin.js
+++ attribute/navbaradmin.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function() {
             icon: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>'
         },
         { 
-            id: 'pengaturan', 
+            id: 'settings', 
             label: 'Setelan', 
             url: 'admin-settings.html', 
             // Ikon Gear/Pengaturan
